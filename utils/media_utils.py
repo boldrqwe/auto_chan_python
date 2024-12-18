34070897a153ac48caa70bb7f7d3f50b5e1d45c7
lalala <lbondyaaa@gmail.com>
2024-12-18 22:01:18 +0300

    version 0.0.14

--- utils/media_utils.py
+++ utils/media_utils.py
@@ -10,13 +10,13 @@ logger = logging.getLogger(__name__)
 
 def create_input_media(url: str, caption: str = None):
     """Определяем тип медиа для отправки в группу."""
-    logger.debug(f"Создание медиа-объекта для URL: {url}, caption: {caption is not None}")
+    # logger.debug(f"Создание медиа-объекта для URL: {url}, caption: {caption is not None}")
     if any(url.endswith(ext) for ext in [".jpg", ".jpeg", ".png", ".gif"]):
         return InputMediaPhoto(media=url, caption=caption, parse_mode='HTML')
     elif any(url.endswith(ext) for ext in [".webm", ".mp4"]):
         return InputMediaVideo(media=url, caption=caption, parse_mode='HTML')
     else:
-        logger.debug("Неизвестный формат. Используем InputMediaPhoto по умолчанию.")
+        # logger.debug("Неизвестный формат. Используем InputMediaPhoto по умолчанию.")
         return InputMediaPhoto(media=url, caption=caption, parse_mode='HTML')
 
 
